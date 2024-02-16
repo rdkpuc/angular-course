99608698c25cd7e0700bf4dd5e0d18551b3a8804
Thiago Gomes <tgteixeira@sga.pucminas.br>
2024-02-16 10:48:45 -0300

    test

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Animal } from '../../Animal';
+import { Car } from '../../Car';
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
@@ -12,14 +13,19 @@ export class ListRenderComponent {
     {name:'Matthew', type: 'Sheep', age: 3}
   ];
 
-  cars = [
-    {model: 'Palio', brand: 'FIAT', engine: '1.0', fuel:'Flex'},
-    {model: 'Gol', brand: 'Volkswagen', engine: '1.0', fuel:'Flex'},
-    {model: 'Corsa', brand: 'Chevrolet', engine: '1.0', fuel:'Flex'}
-  ]
+  cars: Car[] = [
+    {model: 'Palio', brand: 'FIAT', engine: 1.0, year: 2013},
+    {model: 'Onix', brand: 'Chevrolet', engine: 1.4, year: 2020 },
+    {model: 'Jetta', brand: 'Volkswagen', engine: 2.0, year: 2023}
+  ];
 
   animalDetails = '';
   showAge(animal: Animal) {
     this.animalDetails = `The pet ${animal.name} has ${animal.age}y old!`;
+  }
+
+  carDetails = '';
+  showYear(car: Car) {
+    this.carDetails = `This a ${car.brand} ${car.model} year is ${car.year}`;
   }
 }
