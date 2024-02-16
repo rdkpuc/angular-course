6c28153566a66d773f3537535c6c783cfdfd8c2b
Thiago Gomes <tgteixeira@sga.pucminas.br>
2024-02-16 10:36:41 -0300

    application of interfaces

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Animal } from '../../Animal';
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrl: './list-render.component.css'
 })
 export class ListRenderComponent {
-  animals = [
-    {name: 'Harvey', type: 'Dog'},
-    {name: 'Jack', type: 'Pig'},
-    {name:'Matthew', type: 'Sheep'}
+  animals: Animal[] = [
+    {name: 'Harvey', type: 'Dog', age: 6},
+    {name: 'Jack', type: 'Pig', age: 4},
+    {name:'Matthew', type: 'Sheep', age: 3}
   ];
 
   cars = [
@@ -17,4 +17,9 @@ export class ListRenderComponent {
     {model: 'Gol', brand: 'Volkswagen', engine: '1.0', fuel:'Flex'},
     {model: 'Corsa', brand: 'Chevrolet', engine: '1.0', fuel:'Flex'}
   ]
+
+  animalDetails = '';
+  showAge(animal: Animal) {
+    this.animalDetails = `The pet ${animal.name} has ${animal.age}y old!`;
+  }
 }
